e7e628668cc2d7891a4fc95372d6ef7d32fe9dee
quessr <heosera76@gmail.com>
2024-05-28 17:31:03 +0900

    MainActivity에 HomeFragment 연결
    - 프래그먼트를 교체하고 백스택에 추가하는 setFragment 함수 구현

--- app/src/main/java/com/example/finebyme/ui/MainActivity.kt
+++ app/src/main/java/com/example/finebyme/ui/MainActivity.kt
@@ -2,15 +2,30 @@ package com.example.finebyme.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import com.example.finebyme.R
 import com.example.finebyme.data.network.RetrofitInstance
+import com.example.finebyme.databinding.ActivityMainBinding
+import com.example.finebyme.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setFragment(HomeFragment())
 
         RetrofitInstance.fetchRandomPhoto()
         RetrofitInstance.fetchSearchPhoto()
+    }
+
+    private fun setFragment( frag : Fragment ) {
+        supportFragmentManager.commit {
+            replace(R.id.frameLayout, frag)
+            setReorderingAllowed(true)
+            addToBackStack("")
+        }
     }
 }
