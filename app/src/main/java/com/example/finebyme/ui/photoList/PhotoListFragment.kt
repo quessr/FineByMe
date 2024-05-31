7765d4b2d8bf5e4fc49049027ddd2d558bdf78fb
quessr <heosera76@gmail.com>
2024-05-31 13:36:08 +0900

    PhotoList 로딩 상태에 따른 UI 처리 추가
    - PhotoListViewModel 내에 LOADING, DONE, ERROR 상태를 나타내는 State enum 추가
    - 상태 관리를 위한 MutableLiveData _state와 공개 LiveData state 정의
    - 상태 변화를 관찰하고 UI에 반응할 수 있도록 LiveData를 사용하여 상태 노출

--- app/src/main/java/com/example/finebyme/ui/photoList/PhotoListFragment.kt
+++ app/src/main/java/com/example/finebyme/ui/photoList/PhotoListFragment.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.bumptech.glide.Glide
 import com.example.finebyme.R
 import com.example.finebyme.data.network.RetrofitInstance
 import com.example.finebyme.databinding.FragmentHomeBinding
@@ -45,14 +46,35 @@ class PhotoListFragment : Fragment(R.layout.fragment_home) {
         val numberOfColumns = 2
 
 //        recyclerView.layoutManager = StaggeredGridLayoutManager(numberOfColumns, LinearLayoutManager.VERTICAL)
-        val layoutManager = StaggeredGridLayoutManager(numberOfColumns, LinearLayoutManager.VERTICAL)
+        val layoutManager =
+            StaggeredGridLayoutManager(numberOfColumns, LinearLayoutManager.VERTICAL)
         layoutManager.gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_NONE
         recyclerView.layoutManager = layoutManager
 
         binding.recyclerView.adapter = photoAdapter
 
 //        RetrofitInstance.fetchRandomPhoto{ photos -> photos?.let { photoAdapter.setPhoto(it) } }
-        photoListViewModel.photos.observe(viewLifecycleOwner, Observer { photos -> photoAdapter.setPhoto(photos) })
+        photoListViewModel.photos.observe(
+            viewLifecycleOwner,
+            Observer { photos -> photoAdapter.setPhoto(photos) })
+
+        photoListViewModel.state.observe(viewLifecycleOwner, Observer { state ->
+            when (state) {
+                PhotoListViewModel.State.LOADING -> {
+                    Glide.with(this).asGif()
+                        .load(R.drawable.loading).into(binding.imageViewLoading)
+                    binding.imageViewLoading.visibility = View.VISIBLE
+                }
+
+                PhotoListViewModel.State.DONE -> {
+                    binding.imageViewLoading.visibility = View.GONE
+                }
+
+                PhotoListViewModel.State.ERROR -> {
+                    binding.imageViewLoading.visibility = View.GONE
+                }
+            }
+        })
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/finebyme/ui/photoList/PhotoListViewModel.kt
+++ app/src/main/java/com/example/finebyme/ui/photoList/PhotoListViewModel.kt
@@ -7,19 +7,32 @@ import com.example.finebyme.data.model.Photo
 import com.example.finebyme.data.network.RetrofitInstance
 
 class PhotoListViewModel : ViewModel() {
+
+    enum class State {
+        LOADING,
+        DONE,
+        ERROR
+    }
+
     private val _photos: MutableLiveData<List<Photo>> by lazy { MutableLiveData() }
     val photos: LiveData<List<Photo>> get() = _photos
+
+    private val _state: MutableLiveData<State> by lazy { MutableLiveData() }
+    val state: LiveData<State> get() = _state
 
     init {
         fetchPhotos()
     }
 
     private fun fetchPhotos() {
+        _state.postValue(State.LOADING)
         RetrofitInstance.fetchRandomPhoto { photos ->
             if (photos != null) {
                 _photos.postValue(photos)
+                _state.postValue(State.DONE)
             } else {
                 _photos.postValue(listOf())
+                _state.postValue(State.ERROR)
             }
         }
     }
