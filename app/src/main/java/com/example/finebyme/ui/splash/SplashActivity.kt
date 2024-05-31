75dd7bcd6c480ff1432192f1fdbe5a4773fdb1a6
quessr <heosera76@gmail.com>
2024-05-31 15:15:59 +0900

    스플래시 화면 추가

---
+++ app/src/main/java/com/example/finebyme/ui/splash/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.finebyme.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import com.bumptech.glide.Glide
+import com.example.finebyme.R
+import com.example.finebyme.databinding.ActivityMainBinding
+import com.example.finebyme.databinding.ActivitySplashBinding
+import com.example.finebyme.databinding.FragmentPhotoListBinding
+import com.example.finebyme.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    private val binding by lazy { ActivitySplashBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        Glide.with(this).asGif()
+            .load(R.drawable.camera).circleCrop().into(binding.imageViewSplash)
+        binding.imageViewSplash.visibility = View.VISIBLE
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }, 3000)
+    }
+}
