0636aecec3697791cc9416265ab3560cf2520c3b
quessr <heosera76@gmail.com>
2024-06-03 14:30:22 +0900

    favorite_photos 테이블에 대한 PhotoDao 인터페이스 추가

--- app/src/main/java/com/example/finebyme/data/db/PhotoDao.kt
+++ app/src/main/java/com/example/finebyme/data/db/PhotoDao.kt
@@ -1,4 +1,27 @@
 package com.example.finebyme.data.db
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
 interface PhotoDao {
+    @Query("SELECT * from favorite_photos ORDER BY title ASC")
+    fun getAllPhotos(): List<Photo>
+
+    @Query("SELECT * from favorite_photos WHERE id = :id")
+    fun getPhoto(id: Int): Photo
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(photo: Photo)
+
+    @Update
+    suspend fun update(photo: Photo)
+
+    @Delete
+    suspend fun delete(photo: Photo)
+
 }
