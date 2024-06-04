3f614cfbeb0c256d4511a85dd0cbd8f3c0f85d41
quessr <heosera76@gmail.com>
2024-06-04 11:52:27 +0900

    FavoritePhotosDatabase 구현

---
+++ app/src/main/java/com/example/finebyme/data/db/FavoritePhotosDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.finebyme.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Photo::class), version = 1, exportSchema = false)
+public abstract class FavoritePhotosDatabase : RoomDatabase() {
+
+    abstract fun PhotoDao() : PhotoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: FavoritePhotosDatabase?= null
+
+        fun getDatabase(context: Context): FavoritePhotosDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    FavoritePhotosDatabase::class.java,
+                    "favorite_photos_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
