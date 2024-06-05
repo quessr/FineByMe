ce2c3844afd6e19a0eb39242cea0f930f111961d
quessr <heosera76@gmail.com>
2024-06-05 10:49:05 +0900

    BottomNavigationView를 사용하여 하단 네비게이션바 구현
    - PhotoListFragment와 FavoriteListFragment로의 전환 기능 추가

--- app/src/main/java/com/example/finebyme/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/finebyme/ui/main/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.finebyme.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.example.finebyme.R
 import com.example.finebyme.databinding.ActivityMainBinding
+import com.example.finebyme.ui.favoriteList.FavoriteListFragment
+import com.example.finebyme.ui.photoDetail.PhotoDetailFragment
 import com.example.finebyme.ui.photoList.PhotoListFragment
 
 class MainActivity : AppCompatActivity() {
@@ -14,15 +17,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        setFragment(PhotoListFragment())
-
+        showInit()
+        initBottomNav()
     }
 
-    private fun setFragment( frag : Fragment ) {
-        supportFragmentManager.commit {
-            replace(R.id.frameLayout, frag)
-            setReorderingAllowed(true)
-            addToBackStack("")
+    private fun initBottomNav() {
+        binding.bottomNavigation.itemIconTintList = null
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    PhotoListFragment().changeFragment()
+                    Log.d("BottomNav", "Home selected")
+                }
+
+                R.id.nav_favorite -> {
+                    FavoriteListFragment().changeFragment()
+                    Log.d("BottomNav", "Favorite selected")
+                }
+            }
+
+            true
         }
     }
+
+    private fun Fragment.changeFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout, this)
+            .commit()
+    }
+
+    fun showInit() {
+        val transaction = supportFragmentManager.beginTransaction()
+            .add(R.id.frameLayout, PhotoListFragment())
+        transaction.commit()
+    }
+
 }
