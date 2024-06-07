1322614c612da201565e2a266f93a0ecc124f775
quessr <heosera76@gmail.com>
2024-06-07 16:57:17 +0900

    FavoriteListViewModel 구현 및 ViewModel 데이터로 UI 업데이트

--- app/src/main/java/com/example/finebyme/ui/favoriteList/FavoriteListFragment.kt
+++ app/src/main/java/com/example/finebyme/ui/favoriteList/FavoriteListFragment.kt
@@ -5,18 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.example.finebyme.R
-import com.example.finebyme.data.network.RetrofitInstance
 import com.example.finebyme.databinding.FragmentFavoriteListBinding
 import com.example.finebyme.ui.photoList.PhotoAdapter
-import java.lang.Appendable
+import com.example.finebyme.di.AppViewModelFactory
 
 class FavoriteListFragment : Fragment() {
     private lateinit var photoAdapter: PhotoAdapter
-    private var _binding: FragmentFavoriteListBinding?= null
+    private val favoriteListViewModel: FavoriteListViewModel by viewModels {
+        val application = requireActivity().application
+        AppViewModelFactory(application)
+    }
+
+    private var _binding: FragmentFavoriteListBinding? = null
     private val binding get() = _binding!!
     private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,13 +42,18 @@ class FavoriteListFragment : Fragment() {
         recyclerView = binding.recyclerView
 
         val numberOfColumns = 2
-        val layoutManager = StaggeredGridLayoutManager(numberOfColumns, LinearLayoutManager.VERTICAL)
+        val layoutManager =
+            StaggeredGridLayoutManager(numberOfColumns, LinearLayoutManager.VERTICAL)
         layoutManager.gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_NONE
         recyclerView.layoutManager = layoutManager
 
         binding.recyclerView.adapter = photoAdapter
 
-        RetrofitInstance.fetchRandomPhoto { photos -> photos?.let { photoAdapter.setPhoto(it) } }
+//        RetrofitInstance.fetchRandomPhoto { photos -> photos?.let { photoAdapter.setPhoto(it) } }
+
+        favoriteListViewModel.photos.observe(
+            viewLifecycleOwner,
+            { photos -> photoAdapter.setPhoto(photos) })
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/finebyme/ui/favoriteList/FavoriteListViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.finebyme.ui.favoriteList
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.finebyme.data.db.FavoritePhotosDatabase
+import com.example.finebyme.data.db.Photo
+import java.lang.Appendable
+
+class FavoriteListViewModel(application: Application) : AndroidViewModel(application) {
+    private val _photos: MutableLiveData<List<Photo>> by lazy { MutableLiveData() }
+    val photos: LiveData<List<Photo>> get() = _photos
+
+    val context = getApplication<Application>().applicationContext
+    val photoDao = FavoritePhotosDatabase.getDatabase(context).PhotoDao()
+
+    init {
+        fetchFavoritePhotos()
+    }
+
+    private fun fetchFavoritePhotos() {
+        _photos.postValue(photoDao.getAllPhotos())
+    }
+}
