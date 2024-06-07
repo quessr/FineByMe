026c0ead3d71ca28b1453dbdb28ccc46372fdca8
quessr <heosera76@gmail.com>
2024-06-07 16:08:04 +0900

    AppViewModelFactory로 통합 및 다중 ViewModel 지원
    - di폴더 하위로 위치 이동

---
+++ app/src/main/java/com/example/finebyme/di/AppViewModelFactory.kt
@@ -0,0 +1,27 @@
+package com.example.finebyme.di
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.finebyme.data.repository.FavoritePhotosRepository
+import com.example.finebyme.ui.favoriteList.FavoriteListViewModel
+import com.example.finebyme.ui.photoList.PhotoListViewModel
+
+class AppViewModelFactory(
+    private val application: Application,
+    private val favoritePhotosRepository: FavoritePhotosRepository? = null
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(FavoriteListViewModel::class.java) -> {
+                FavoriteListViewModel(application) as T
+            }
+
+            modelClass.isAssignableFrom(PhotoListViewModel::class.java) -> {
+                favoritePhotosRepository?.let { PhotoListViewModel(application, it) } as T
+            }
+
+            else -> throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
+}
