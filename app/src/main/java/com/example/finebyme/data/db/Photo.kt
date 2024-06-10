fedeaafea06743879d75ab22b8e8f4ab80f74afd
quessr <heosera76@gmail.com>
2024-06-10 10:53:20 +0900

    kotlin-parcelize 플러그인 설정 및 Photo 데이터 클래스에 Parcelable 구현
    - Photo 데이터 클래스에 @Parcelize 어노테이션 추가

--- app/src/main/java/com/example/finebyme/data/db/Photo.kt
+++ app/src/main/java/com/example/finebyme/data/db/Photo.kt
@@ -1,9 +1,13 @@
 package com.example.finebyme.data.db
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.versionedparcelable.VersionedParcelize
+import kotlinx.parcelize.Parcelize
 
 //사용자가 즐겨찾기한 사진들
+@Parcelize
 @Entity(tableName = "favorite_photos")
 data class Photo(
     @PrimaryKey(autoGenerate = true)
@@ -12,4 +16,4 @@ data class Photo(
     val description: String?,
     val fullUrl: String,
     val thumbUrl: String,
-)
+) : Parcelable
