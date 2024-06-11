df5a9ab019f5553b61d9422b6f7dd8d92f35a0aa
quessr <heosera76@gmail.com>
2024-06-11 15:28:51 +0900

    photoListViewModel의 상태를 이용하여 로딩 상태에 따라 UI 업데이트 로직 구현
    - 로딩 중에는 로딩 이미지를 보여주고, 로딩 완료 후에는 사진을 보여주도록 구현

--- app/src/main/java/com/example/finebyme/ui/photoDetail/PhotoDetailActivity.kt
+++ app/src/main/java/com/example/finebyme/ui/photoDetail/PhotoDetailActivity.kt
@@ -2,14 +2,23 @@ package com.example.finebyme.ui.photoDetail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 import com.example.finebyme.R
+import com.example.finebyme.data.db.FavoritePhotosDatabase
 import com.example.finebyme.data.db.Photo
+import com.example.finebyme.data.repository.FavoritePhotosImpl
 import com.example.finebyme.databinding.ActivityPhotoDetailBinding
+import com.example.finebyme.di.AppViewModelFactory
+import com.example.finebyme.ui.photoList.PhotoListViewModel
 
 class PhotoDetailActivity : AppCompatActivity() {
 
@@ -20,6 +29,13 @@ class PhotoDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPhotoDetailBinding
     private var photo: Photo? = null
     private lateinit var viewModel: PhotoDetailViewModel
+    private val photoListViewModel: PhotoListViewModel by viewModels {
+        val application = this.application
+        val photoDao = FavoritePhotosDatabase.getDatabase(application).PhotoDao()
+        val favoritePhotosRepository = FavoritePhotosImpl(photoDao)
+        AppViewModelFactory(application, favoritePhotosRepository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPhotoDetailBinding.inflate(layoutInflater)
@@ -29,21 +45,49 @@ class PhotoDetailActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this).get(PhotoDetailViewModel::class.java)
 
         setupPhotoDetails()
+
+        photoListViewModel.state.observe(this, Observer { state ->
+            when (state) {
+                PhotoListViewModel.State.LOADING -> showLoading()
+                PhotoListViewModel.State.DONE -> setupPhotoDetails()
+                PhotoListViewModel.State.ERROR -> showError()
+            }
+        })
     }
 
     private fun setupPhotoDetails() {
         photo?.let {
             Glide.with(this)
-                .load(it?.fullUrl)
+                .load(it.fullUrl)
                 .centerCrop()
                 .transition(DrawableTransitionOptions.withCrossFade())
                 .into(binding.ivPhoto)
 
-            val transformedTitle = it.title?.let {title ->
+            val transformedTitle = it.title?.let { title ->
                 viewModel.transformTitle(title)
             }
             binding.tvTitle.text = transformedTitle
             binding.tvDescription.text = it?.description
         }
+    }
+
+    private fun showLoading() {
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.loading)
+            .centerCrop()
+            .override(40, 40)
+            .into(binding.ivLoading)
+        binding.ivLoading.visibility = View.VISIBLE
+        binding.root.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                R.color.white
+            )
+        )
+    }
+
+    private fun showError() {
+        // TODO
     }
 }
