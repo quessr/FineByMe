60db42d6c82d20b5021786fa44accda016e4dec2
quessr <heosera76@gmail.com>
2024-06-12 15:37:01 +0900

    ViewModel에 LiveData 추가 및 제목 변환 로직 개선
    - `MutableLiveData<Photo>`와 `LiveData<Photo>`를 `PhotoDetailViewModel`에 추가하여 변환된 사진 데이터를 LiveData로 관리하도록 수정
    - `onEntryScreen(photo: Photo)` 메서드를 추가하여 입력된 사진 데이터를 변환하고 LiveData에 업데이트하도록 구현

--- app/src/main/java/com/example/finebyme/ui/photoDetail/PhotoDetailViewModel.kt
+++ app/src/main/java/com/example/finebyme/ui/photoDetail/PhotoDetailViewModel.kt
@@ -1,8 +1,20 @@
 package com.example.finebyme.ui.photoDetail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.finebyme.data.db.Photo
 
 class PhotoDetailViewModel : ViewModel() {
+    private val _transformedPhoto = MutableLiveData<Photo>()
+    val transformedPhoto: LiveData<Photo> get() = _transformedPhoto
+
+    fun onEntryScreen(photo: Photo) {
+        val transformTitle = transformTitle(photo.title)
+        val transformedPhoto = photo.copy(title = transformTitle)
+        _transformedPhoto.value = transformedPhoto
+    }
+
     fun transformTitle(title: String): String {
         return title.replace("-", " ")
             .split(" ")
