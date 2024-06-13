3f42567ba491e2dfc59ca7d42252876bd9854efe
quessr <heosera76@gmail.com>
2024-06-13 11:33:29 +0900

    PhotoDetailViewModel 로딩상태 관리 기능 추가 및 코드 정리
    - State enum 추가 (LOADING, DONE, ERROR 상태 정의)
    - 상태 관리를 위한 _state MutableLiveData 및 state LiveData 추가
    - init 블록에서 초기 상태를 LOADING으로 설정
    - 사진 로드 완료 및 실패 시 상태 업데이트를 위한 onPhotoLoadCompleted와 onPhotoLoadFail 메서드 추가
    - transformTitle 메서드를 private으로 변경
    - 코드 정리 및 불필요한 공백 제거

--- app/src/main/java/com/example/finebyme/ui/photoDetail/PhotoDetailViewModel.kt
+++ app/src/main/java/com/example/finebyme/ui/photoDetail/PhotoDetailViewModel.kt
@@ -4,30 +4,51 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.finebyme.data.db.Photo
+import com.example.finebyme.ui.photoList.PhotoListViewModel
 
 class PhotoDetailViewModel : ViewModel() {
+    enum class State {
+        LOADING,
+        DONE,
+        ERROR
+    }
+
     private val _transformedPhoto = MutableLiveData<Photo>()
     val transformedPhoto: LiveData<Photo> get() = _transformedPhoto
 
     private val _isFavorite = MutableLiveData<Boolean>()
-    val isFavorite : LiveData<Boolean> get() = _isFavorite
+    val isFavorite: LiveData<Boolean> get() = _isFavorite
+
+    private val _state: MutableLiveData<State> by lazy { MutableLiveData() }
+    val state: LiveData<State> get() = _state
+
+    init {
+        _state.value = State.LOADING
+    }
+
+    fun onPhotoLoadCompleted() {
+        _state.value = State.DONE
+    }
+
+    fun onPhotoLoadFail() {
+        _state.value = State.ERROR
+    }
 
     fun onEntryScreen(photo: Photo) {
         val transformTitle = transformTitle(photo.title)
-        val transformedPhoto = photo.copy(title = transformTitle)
-        _transformedPhoto.value = transformedPhoto
         photo.title = transformTitle
         _transformedPhoto.value = photo
     }
 
-    fun transformTitle(title: String): String {
+    fun toggleFavorite() {
+
+        _isFavorite.value = !(_isFavorite.value ?: false)
+    }
+
+    private fun transformTitle(title: String): String {
         return title.replace("-", " ")
             .split(" ")
             .dropLast(1)
             .joinToString(" ")
-    }
-
-    fun toggleFavorite() {
-        _isFavorite.value = !(_isFavorite.value ?: false)
     }
 }
