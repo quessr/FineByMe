eec9221d3379ed602784bd4ef79995f6e625e8a9
quessr <heosera76@gmail.com>
2024-06-27 12:33:52 +0900

    즐겨찾기 목록이 비어 있을 때 tvEmpty를 표시하도록 변경

--- app/src/main/java/com/example/finebyme/ui/favoriteList/FavoriteListFragment.kt
+++ app/src/main/java/com/example/finebyme/ui/favoriteList/FavoriteListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -70,7 +71,10 @@ class FavoriteListFragment : Fragment() {
 
         favoriteListViewModel.photos.observe(
             viewLifecycleOwner
-        ) { photos -> photoAdapter.setPhoto(photos) }
+        ) { photos ->
+            photoAdapter.setPhoto(photos)
+            binding.tvEmpty.isVisible = photos.isEmpty()
+        }
 
         photoAdapter.setOnPhotoClickListener(object : PhotoAdapter.OnPhotoClickListener {
             override fun onPhotoClick(photo: Photo) {
