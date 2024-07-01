33ce863abdd32cc3c6773cdb7aadbfe63b1735c8
quessr <heosera76@gmail.com>
2024-07-01 15:20:41 +0900

    사진 데이터를 관리하는 PhotoRepository 클래스 추가 및 리팩토링

---
+++ app/src/main/java/com/example/finebyme/data/repository/PhotoRepository.kt
@@ -0,0 +1,41 @@
+package com.example.finebyme.data.repository
+
+import androidx.lifecycle.MutableLiveData
+import com.example.finebyme.data.datasource.UnSplashDataSource
+import com.example.finebyme.data.datasource.UserDataSource
+import com.example.finebyme.data.db.Photo
+import com.example.finebyme.data.model.SearchPhotoResponse
+import com.example.finebyme.data.model.UnsplashPhoto
+
+class PhotoRepository(
+    private val unsplashDataSource: UnSplashDataSource,
+    private val userDataSource: UserDataSource
+) {
+    fun getRandomPhotoList(onResult: (List<UnsplashPhoto>?) -> Unit) {
+        unsplashDataSource.getRandomPhotoList(onResult)
+    }
+
+    fun getSearchPhotoList(query: String): MutableLiveData<SearchPhotoResponse> {
+        return unsplashDataSource.getSearchPhotoList(query)
+    }
+
+    fun getFavoritePhotoList(): List<Photo> {
+        return userDataSource.getAllPhotos()
+    }
+
+    fun getFavoritePhoto(id: String): Photo {
+        return userDataSource.getPhoto(id)
+    }
+
+    fun addPhotoToFavorites(photo: Photo) {
+        userDataSource.insertPhoto(photo)
+    }
+
+    fun removePhotoFromFavorites(photo: Photo) {
+        userDataSource.deletePhoto(photo)
+    }
+
+    fun isPhotoFavorite(photoId: String): Boolean {
+        return userDataSource.isPhotoFavorite(photoId)
+    }
+}
