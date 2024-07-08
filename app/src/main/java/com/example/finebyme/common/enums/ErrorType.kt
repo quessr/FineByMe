7a8b46d6c8088f50ee4b2f3a291640cadb19df7a
quessr <heosera76@gmail.com>
2024-07-08 17:08:12 +0900

    ErrorType enum 클래스 추가
    - 다양한 에러 타입 및 메시지 정의

---
+++ app/src/main/java/com/example/finebyme/common/enums/ErrorType.kt
@@ -0,0 +1,12 @@
+package com.example.finebyme.common.enums
+
+enum class ErrorType(val message: String) {
+    NETWORK_ERROR("Network error. Please try again."),
+    BAD_REQUEST("Bad Request: The request was unacceptable, often due to missing a required parameter."),
+    UNAUTHORIZED("Unauthorized: Invalid Access Token."),
+    FORBIDDEN("Forbidden: Missing permissions to perform request."),
+    NOT_FOUND("Not Found: The requested resource doesn’t exist."),
+    SERVER_ERROR("Server Error: Something went wrong on our end. Please try again later."),
+    NO_INTERNET("No internet connection. Please check your connection."),
+    UNKNOWN_ERROR("An unknown error occurred. Please try again.")
+}
