8936694e51431edd5c8a0b9ef9f9add20d8ac84b
quessr <heosera76@gmail.com>
2024-07-08 12:22:43 +0900

    서버 응답 실패 시 HttpException을 사용하여 onFailure 호출 추가

--- app/src/main/java/com/example/finebyme/utils/NetworkUtils.kt
+++ app/src/main/java/com/example/finebyme/utils/NetworkUtils.kt
@@ -3,6 +3,7 @@ package com.example.finebyme.utils
 import android.util.Log
 import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.HttpException
 import retrofit2.Response
 
 
@@ -11,13 +12,19 @@ object NetworkUtils {
         call.enqueue(object  : Callback<T> {
             override fun onResponse(call: Call<T>, response: Response<T>) {
                 if (response.isSuccessful) {
+                    // HTTP 응답 코드가 2xx인 경우
                     onSuccess(response)
-                } else {
-                    Log.e("NetworkUtils", "Error: ${response.errorBody()?.string()}")
+                }  else {
+                    // HTTP 응답 코드가 4xx, 5xx인 경우
+                    // 서버 응답이 성공적이지 않을 때 HttpException을 생성하여 onFailure 콜백을 호출합니다.
+                    val httpException = HttpException(response)
+                    Log.e("NetworkUtils", "Error: ${httpException.message()}")
+                    onFailure(httpException)
                 }
             }
 
             override fun onFailure(call: Call<T>, t: Throwable) {
+                // 네트워크 문제 또는 요청 중 예외 발생
                 onFailure(t)
             }
         })
