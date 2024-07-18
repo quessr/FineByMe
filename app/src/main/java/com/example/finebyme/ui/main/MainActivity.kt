94849b0192f01c882c4a8c3bdbd11bd5a6d5ad88
quessr <heosera76@gmail.com>
2024-07-18 14:27:16 +0900

    Navigation Component로 백 스택 관리 및 종료 확인 다이얼로그 로직 수정
    - 불필요한 코드 정리

--- app/src/main/java/com/example/finebyme/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/finebyme/ui/main/MainActivity.kt
@@ -14,7 +14,7 @@ import com.example.finebyme.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private lateinit var navController: NavController
-    private var isBackStackEmpty = true
+    private lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -24,76 +24,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupNavigation() {
-        val navHostFragment =
+        navHostFragment =
             supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.navController
         NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
-//        binding.bottomNavigation.setupWithNavController(navController)
 
         navHostFragment.childFragmentManager.addOnBackStackChangedListener {
-            isBackStackEmpty = if (navHostFragment.childFragmentManager.backStackEntryCount == 0) {
-                Log.d("fbm backstack init", "fbm backstack init ${navHostFragment.childFragmentManager.backStackEntryCount.toString()}")
-                true
-            } else {
                 Log.d("fbm backstack", navHostFragment.childFragmentManager.backStackEntryCount.toString())
-                false
-            }
         }
     }
 
     private fun handleOnBackPressed() {
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-
-                if (isBackStackEmpty) {
+                if (navHostFragment.childFragmentManager.backStackEntryCount == 0) {
                     Log.d("fbm backstack", "isBackStackEmpty")
 
                     showExitConfirmationDialog()
                 } else {
+                    Log.d("fbm backstack", navHostFragment.childFragmentManager.backStackEntryCount.toString())
                     navController.popBackStack()
                 }
             }
         })
     }
-
-
-//    private fun handleOnBackPressed() {
-//        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
-//            override fun handleOnBackPressed() {
-//
-//                if (supportFragmentManager.backStackEntryCount == 1) {
-//                    showExitConfirmationDialog()
-//                } else {
-//                    supportFragmentManager.popBackStack()
-//
-//                    supportFragmentManager.addOnBackStackChangedListener(object :
-//                        FragmentManager.OnBackStackChangedListener {
-//                        override fun onBackStackChanged() {
-//                            supportFragmentManager.removeOnBackStackChangedListener(this)
-//
-//                            val currentFragment =
-//                                supportFragmentManager.findFragmentById(R.id.navHostFragment)
-//
-//                            Log.d("@@@@@@", "currentFragment : $currentFragment")
-//                            when (currentFragment) {
-//                                is PhotoListFragment -> {
-//                                    binding.bottomNavigation.menu.findItem(R.id.nav_home).isChecked =
-//                                        true
-//                                }
-//
-//                                is FavoriteListFragment -> {
-//                                    binding.bottomNavigation.menu.findItem(R.id.nav_favorite).isChecked =
-//                                        true
-//                                }
-//                            }
-//
-//                        }
-//
-//                    })
-//                }
-//            }
-//        })
-//    }
 
     private fun showExitConfirmationDialog() {
         AlertDialog.Builder(this)
@@ -107,50 +61,4 @@ class MainActivity : AppCompatActivity() {
             }
             .show()
     }
-
-//    private fun initBottomNav() {
-//        binding.bottomNavigation.itemIconTintList = null
-//
-//        binding.bottomNavigation.setOnItemSelectedListener {
-//            when (it.itemId) {
-//                R.id.nav_home -> {
-//                    changeFragment(PhotoListFragment::class.java.name)
-//                    Log.d("BottomNav", "Home selected")
-//                }
-//
-//                R.id.nav_favorite -> {
-//                    changeFragment(FavoriteListFragment::class.java.name)
-//                    Log.d("BottomNav", "Favorite selected")
-//                }
-//            }
-//
-//            true
-//        }
-//    }
-
-//    private fun changeFragment(tag: String) {
-//        // 현재 프래그먼트 찾기
-//        var fragment = supportFragmentManager.findFragmentByTag(tag)
-//
-//        if (fragment == null) {
-//            fragment = when (tag) {
-//                PhotoListFragment::class.java.name -> PhotoListFragment()
-//                FavoriteListFragment::class.java.name -> FavoriteListFragment()
-//                else -> throw IllegalStateException("Unknown fragment tag: $tag")
-//            }
-//        }
-//
-//        supportFragmentManager.beginTransaction()
-//            .replace(R.id.navHostFragment, fragment, tag)
-//            .setReorderingAllowed(true)
-//            .addToBackStack(tag)
-//            .commit()
-//
-//        Log.d("tag : ", tag)
-//    }
-//
-//    private fun showInit() {
-//        changeFragment(PhotoListFragment::class.java.name)
-//    }
-
 }
