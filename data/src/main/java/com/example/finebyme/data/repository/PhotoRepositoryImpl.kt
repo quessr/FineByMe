87c79c378f1dd14b5f2833723e309541a9e4975e
quessr <heosera76@gmail.com>
2024-07-24 17:40:40 +0900

    getRandomPhotoList 및 getSearchPhotoList 구현 및 PhotoMapper 적용

--- data/src/main/java/com/example/finebyme/data/repository/PhotoRepositoryImpl.kt
+++ data/src/main/java/com/example/finebyme/data/repository/PhotoRepositoryImpl.kt
@@ -2,7 +2,7 @@ package com.example.finebyme.data.repository
 
 import com.example.finebyme.data.datasource.UnSplashDataSource
 import com.example.finebyme.data.datasource.UserDataSource
-import com.example.finebyme.data.model.UnsplashPhoto
+import com.example.finebyme.data.mapper.PhotoMapper
 import com.example.finebyme.domain.entity.Photo
 import com.example.finebyme.domain.repositoryInterface.PhotoRepository
 import javax.inject.Inject
@@ -10,31 +10,44 @@ import javax.inject.Inject
 open class PhotoRepositoryImpl @Inject constructor(
     private val unsplashDataSource: UnSplashDataSource,
     private val userDataSource: UserDataSource
-): PhotoRepository {
+) : PhotoRepository {
 
 
     override fun getRandomPhotoList(onResult: (Result<List<Photo>>) -> Unit) {
-        // unsplashDataSource.getRandomPhotoList(onResult)
+//         unsplashDataSource.getRandomPhotoList(onResult)
+        unsplashDataSource.getRandomPhotoList { result ->
+            result?.onSuccess { unsplashPhotos ->
+                val photos = PhotoMapper.mapToPhotoList(unsplashPhotos)
+                onResult(Result.success(photos))
+            }?.onFailure { throwable ->
+                onResult(Result.failure(throwable))
+            }
+        }
     }
 
     override fun getSearchPhotoList(query: String, onResult: (Result<List<Photo>>) -> Unit) {
-        TODO("Not yet implemented")
+        unsplashDataSource.getSearchPhotoList(query) { result ->
+            result?.onSuccess { unsplashPhtos ->
+                val photos = PhotoMapper.mapToPhotoList(unsplashPhtos)
+                onResult(Result.success(photos))
+            }?.onFailure { throwable ->
+                onResult(Result.failure(throwable))
+            }
+        }
     }
 
     override fun getFavoritePhotoList(): List<Photo> {
-        TODO("Not yet implemented")
+        return emptyList()
     }
 
     override fun addPhotoToFavorites(photo: Photo) {
-        TODO("Not yet implemented")
     }
 
     override fun removePhotoFromFavorites(photo: Photo) {
-        TODO("Not yet implemented")
     }
 
     override fun isPhotoFavorite(photoId: String): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 
 
