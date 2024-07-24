c0ee8c33c279aec224f3c7928c1d75796cdb01f4
quessr <heosera76@gmail.com>
2024-07-24 17:11:04 +0900

    ErrorType 및 ErrorHandler를 데이터 모듈로 이동

--- data/src/main/java/com/example/finebyme/data/common/enums/ErrorType.kt
+++ data/src/main/java/com/example/finebyme/data/common/enums/ErrorType.kt
@@ -1,4 +1,4 @@
-package com.example.finebyme.presentation.common.enums
+package com.example.finebyme.data.common.enums
 
 enum class ErrorType(val message: String) {
     NETWORK_ERROR("Network error. Please try again."),
