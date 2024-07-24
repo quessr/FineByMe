9f812209303c4628c9f801afcebc32c65cb396c2
quessr <heosera76@gmail.com>
2024-07-24 17:14:02 +0900

    enqueueCall 함수의 onFailure 콜백을 String 타입으로 수정 및 ErrorHandler를 통한 에러 메시지 처리 로직 추가

--- data/src/main/java/com/example/finebyme/data/utils/NetworkUtils.kt
+++ data/src/main/java/com/example/finebyme/data/utils/NetworkUtils.kt
@@ -8,7 +8,7 @@ import retrofit2.Response
 
 
 object NetworkUtils {
-    fun <T> enqueueCall(call: Call<T>, onSuccess: (Response<T>) -> Unit, onFailure: (Throwable) -> Unit) {
+    fun <T> enqueueCall(call: Call<T>, onSuccess: (Response<T>) -> Unit, onFailure: (String) -> Unit) {
         call.enqueue(object  : Callback<T> {
             override fun onResponse(call: Call<T>, response: Response<T>) {
                 if (response.isSuccessful) {
@@ -19,13 +19,15 @@ object NetworkUtils {
                     // 서버 응답이 성공적이지 않을 때 HttpException을 생성하여 onFailure 콜백을 호출합니다.
                     val httpException = HttpException(response)
                     Log.e("NetworkUtils", "Error: ${httpException.message()}")
-                    onFailure(httpException)
+                    val errorMessage = ErrorHandler.handleFailure(httpException)
+                    onFailure(errorMessage)
                 }
             }
 
             override fun onFailure(call: Call<T>, t: Throwable) {
                 // 네트워크 문제 또는 요청 중 예외 발생
-                onFailure(t)
+                val errorMessage = ErrorHandler.handleFailure(t)
+                onFailure(errorMessage)
             }
         })
     }
