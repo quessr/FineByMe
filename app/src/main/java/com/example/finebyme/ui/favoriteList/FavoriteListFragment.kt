e4c400a88f466cf44178226d9ed2ebefc8451bca
quessr <heosera76@gmail.com>
2024-07-29 16:28:57 +0900

    RecyclerView 상태 저장 및 복원 기능 추가
    - 사진 상세 화면에서 즐겨찾기를 해제 후 뒤로 가기를 눌러 다시 즐겨찾기 화면으로 돌아오면 빈 화면이 보여지는 이슈 수정
    - 즐겨찾기 화면에서 즐겨찾기 항목이 추가될 때 스크롤이 아래로 밀리는 이슈 수정

--- app/src/main/java/com/example/finebyme/ui/favoriteList/FavoriteListFragment.kt
+++ app/src/main/java/com/example/finebyme/ui/favoriteList/FavoriteListFragment.kt
@@ -1,6 +1,7 @@
 package com.example.finebyme.ui.favoriteList
 
 import android.os.Bundle
+import android.os.Parcelable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -21,8 +22,8 @@ import com.example.finebyme.databinding.FragmentFavoriteListBinding
 import com.example.finebyme.di.AppViewModelFactory
 import com.example.finebyme.ui.photoList.PhotoAdapter
 import com.example.finebyme.utils.IntentUtils.newPhotoDetail
-
 class FavoriteListFragment : Fragment() {
+
     private lateinit var photoAdapter: PhotoAdapter
     private val favoriteListViewModel: FavoriteListViewModel by activityViewModels {
         val application = requireActivity().application
@@ -33,11 +34,13 @@ class FavoriteListFragment : Fragment() {
         val photoRepository = PhotoRepository(unSplashDataSource, userDataSource)
         AppViewModelFactory(application, photoRepository)
     }
+    private var recyclerViewState: Parcelable? = null
 
     private var _binding: FragmentFavoriteListBinding? = null
     private val binding get() = _binding!!
     private lateinit var recyclerView: RecyclerView
     private var isPass = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -61,10 +64,11 @@ class FavoriteListFragment : Fragment() {
 
         val numberOfColumns = 2
         val layoutManager =
-            StaggeredGridLayoutManager(numberOfColumns, LinearLayoutManager.VERTICAL)
-        layoutManager.gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
-        recyclerView.layoutManager = layoutManager
+            StaggeredGridLayoutManager(numberOfColumns, LinearLayoutManager.VERTICAL).apply {
+                gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
+            }
 
+        recyclerView.layoutManager = layoutManager
         binding.recyclerView.adapter = photoAdapter
     }
 
@@ -72,10 +76,13 @@ class FavoriteListFragment : Fragment() {
         favoriteListViewModel.photos.observe(
             viewLifecycleOwner
         ) { photos ->
-            val titles = photos.joinToString(", ") { it.title }
-            Log.d("fmb Fragment", "Observed photo titles: $titles")
             photoAdapter.submitList(photos) {
-                recyclerView.scrollToPosition(0)
+                if (recyclerViewState != null) {
+                    recyclerView.layoutManager?.onRestoreInstanceState(recyclerViewState)
+                }
+                else {
+                    recyclerView.scrollToPosition(0)
+                }
             }
             binding.tvEmpty.isVisible = photos.isEmpty()
         }
@@ -90,6 +97,11 @@ class FavoriteListFragment : Fragment() {
         })
     }
 
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        recyclerView.layoutManager?.onRestoreInstanceState(recyclerViewState)
+    }
+
     override fun onResume() {
         super.onResume()
         if (isPass) {
@@ -97,6 +109,11 @@ class FavoriteListFragment : Fragment() {
             return
         }
         favoriteListViewModel.onResumeScreen()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        recyclerViewState = recyclerView.layoutManager?.onSaveInstanceState()
     }
 
     override fun onDestroyView() {
