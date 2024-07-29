0054adb0dbe1e0d8eb0976db3b879d78add6b90f
quessr <heosera76@gmail.com>
2024-07-29 16:49:22 +0900

    데이터베이스 및 도메인 간 Entity 변환 함수 추가

--- data/src/main/java/com/example/finebyme/data/mapper/PhotoMapper.kt
+++ data/src/main/java/com/example/finebyme/data/mapper/PhotoMapper.kt
@@ -17,4 +17,34 @@ object PhotoMapper {
 
     fun mapToPhotoList(unsplashPhotoList: List<UnsplashPhoto>): List<Photo> =
         unsplashPhotoList.map { mapToPhoto(it) }
+
+    private fun mapToDomainPhoto(dataPhoto: com.example.finebyme.data.db.Photo): Photo =
+        dataPhoto.run {
+            Photo(
+                id = id,
+                title = title,
+                description = description,
+                fullUrl = fullUrl,
+                thumbUrl = thumbUrl
+            )
+        }
+
+    fun mapToDomainPhotoList(dataPhotoList: List<com.example.finebyme.data.db.Photo>): List<Photo> =
+        dataPhotoList.map { mapToDomainPhoto(it) }
+
+    fun mapToDataPhoto(domainPhoto: Photo): com.example.finebyme.data.db.Photo =
+        domainPhoto.run {
+            com.example.finebyme.data.db.Photo(
+                id = id,
+                title = title,
+                description = description,
+                fullUrl = fullUrl,
+                thumbUrl = thumbUrl,
+                inputAt = System.currentTimeMillis() // 필요한 경우 적절한 값을 설정
+            )
+        }
+
+    fun mapToDataPhotoList(domainPhotoList: List<Photo>): List<com.example.finebyme.data.db.Photo> =
+        domainPhotoList.map { mapToDataPhoto(it) }
+
 }
