b3bf159d02b69af8fd10803e3d74b9d126aeca64
quessr <heosera76@gmail.com>
2025-04-07 14:56:18 +0900

    즐겨찾기 리스트 화면을 Compose 기반으로 변경 및 PhotoGrid 컴포넌트 적용

--- presentation/src/main/java/com/example/finebyme/presentation/favoriteList/FavoriteListFragment.kt
+++ presentation/src/main/java/com/example/finebyme/presentation/favoriteList/FavoriteListFragment.kt
@@ -2,18 +2,16 @@ package com.example.finebyme.presentation.favoriteList
 
 import android.os.Bundle
 import android.os.Parcelable
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.example.finebyme.domain.entity.Photo
-import com.example.finebyme.domain.interaction.OnPhotoClickListener
+import com.example.finebyme.presentation.common.component.PhotoGrid
 import com.example.finebyme.presentation.databinding.FragmentFavoriteListBinding
 import com.example.finebyme.presentation.photoList.PhotoAdapter
 import com.example.finebyme.presentation.utils.IntentUtils.newPhotoDetail
@@ -34,61 +32,73 @@ class FavoriteListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentFavoriteListBinding.inflate(inflater)
-        return binding.root
+//        _binding = FragmentFavoriteListBinding.inflate(inflater)
+//        return binding.root
+        return ComposeView(requireContext()).apply {
+            setContent {
+                val photos by favoriteListViewModel.photos.observeAsState(emptyList())
+                PhotoGrid(
+                    photos = photos,
+                    onClick = { photo ->
+                        val intent = newPhotoDetail(requireContext(), photo)
+                        startActivity(intent)
+                    }
+                )
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setupRecyclerview()
-        setupObservers()
-        setupListeners()
+//        setupRecyclerview()
+//        setupObservers()
+//        setupListeners()
 
     }
 
-    private fun setupRecyclerview() {
-        photoAdapter = PhotoAdapter(favoriteListViewModel)
-        recyclerView = binding.recyclerView
+    //TODO StaggeredGridLayout
+//    private fun setupRecyclerview() {
+//        photoAdapter = PhotoAdapter(favoriteListViewModel)
+//        recyclerView = binding.recyclerView
+//
+//        val numberOfColumns = 2
+//        val layoutManager =
+//            StaggeredGridLayoutManager(numberOfColumns, LinearLayoutManager.VERTICAL).apply {
+//                gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
+//            }
+//
+//        recyclerView.layoutManager = layoutManager
+//        binding.recyclerView.adapter = photoAdapter
+//    }
+//
+//    private fun setupObservers() {
+//        favoriteListViewModel.photos.observe(
+//            viewLifecycleOwner
+//        ) { photos ->
+//            photoAdapter.submitList(photos) {
+//                if (recyclerViewState != null) {
+//                    recyclerView.layoutManager?.onRestoreInstanceState(recyclerViewState)
+//                } else {
+//                    recyclerView.scrollToPosition(0)
+//                }
+//            }
+//            binding.tvEmpty.isVisible = photos.isEmpty()
+//        }
+//    }
 
-        val numberOfColumns = 2
-        val layoutManager =
-            StaggeredGridLayoutManager(numberOfColumns, LinearLayoutManager.VERTICAL).apply {
-                gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
-            }
-
-        recyclerView.layoutManager = layoutManager
-        binding.recyclerView.adapter = photoAdapter
-    }
-
-    private fun setupObservers() {
-        favoriteListViewModel.photos.observe(
-            viewLifecycleOwner
-        ) { photos ->
-            photoAdapter.submitList(photos) {
-                if (recyclerViewState != null) {
-                    recyclerView.layoutManager?.onRestoreInstanceState(recyclerViewState)
-                }
-                else {
-                    recyclerView.scrollToPosition(0)
-                }
-            }
-            binding.tvEmpty.isVisible = photos.isEmpty()
-        }
-    }
-
-    private fun setupListeners() {
-        photoAdapter.setOnPhotoClickListener(object : OnPhotoClickListener {
-            override fun onPhotoClick(photo: Photo) {
-                val intent = newPhotoDetail(requireContext(), photo)
-                startActivity(intent)
-            }
-        })
-    }
+//    private fun setupListeners() {
+//        photoAdapter.setOnPhotoClickListener(object : OnPhotoClickListener {
+//            override fun onPhotoClick(photo: Photo) {
+//                val intent = newPhotoDetail(requireContext(), photo)
+//                startActivity(intent)
+//            }
+//        })
+//    }
 
     override fun onViewStateRestored(savedInstanceState: Bundle?) {
         super.onViewStateRestored(savedInstanceState)
-        recyclerView.layoutManager?.onRestoreInstanceState(recyclerViewState)
+//        recyclerView.layoutManager?.onRestoreInstanceState(recyclerViewState)
     }
 
     override fun onResume() {
@@ -102,7 +112,7 @@ class FavoriteListFragment : Fragment() {
 
     override fun onPause() {
         super.onPause()
-        recyclerViewState = recyclerView.layoutManager?.onSaveInstanceState()
+//        recyclerViewState = recyclerView.layoutManager?.onSaveInstanceState()
     }
 
     override fun onDestroyView() {
