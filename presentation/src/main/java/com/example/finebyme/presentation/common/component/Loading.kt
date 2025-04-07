cc619d3f79cf6a04b69e343f6406fccc62f1809f
quessr <heosera76@gmail.com>
2025-04-07 16:48:04 +0900

    GIF 애니메이션을 지원하는 Loading 컴포저블 공통화

---
+++ presentation/src/main/java/com/example/finebyme/presentation/common/component/Loading.kt
@@ -0,0 +1,45 @@
+package com.example.finebyme.presentation.common.component
+
+import android.os.Build
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.ImageRequest
+import com.example.finebyme.presentation.R
+
+@Composable
+fun Loading(
+    modifier: Modifier = Modifier,
+    size: Int = 50
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(R.drawable.loading)
+                .decoderFactory(
+                    if (Build.VERSION.SDK_INT >= 28) {
+                        ImageDecoderDecoder.Factory()
+                    } else {
+                        GifDecoder.Factory()
+                    }
+                ).build(),
+            contentDescription = "로딩 중",
+            modifier = Modifier
+                .size(size.dp)
+                .clip(CircleShape)
+        )
+    }
+}
