5ae6802be6f2adf7b2dbcb8a0c86b23971333997
quessr <heosera76@gmail.com>
2025-04-16 15:25:23 +0900

    MainActivity를 Compose 기반 구조로 전환

--- presentation/src/main/java/com/example/finebyme/presentation/main/MainActivity.kt
+++ presentation/src/main/java/com/example/finebyme/presentation/main/MainActivity.kt
@@ -6,44 +6,68 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.finebyme.presentation.R
 import com.example.finebyme.presentation.databinding.ActivityMainBinding
+import com.example.finebyme.presentation.navigation.BottomNavigationBar
+import com.example.finebyme.presentation.navigation.FbmNavGraph
+import com.example.finebyme.presentation.photoList.PhotoListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    //    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private lateinit var navController: NavController
     private lateinit var navHostFragment: NavHostFragment
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+//        setContentView(binding.root)
 
-        setupNavigation()
+        setContent {
+            MainScreen()
+        }
+
+//        setupNavigation()
         handleOnBackPressed()
 
-        val cursor = contentResolver.query(Uri.parse("content://com.fbm.contentprovider/favorite"), null, null, null)
+        val cursor = contentResolver.query(
+            Uri.parse("content://com.fbm.contentprovider/favorite"),
+            null,
+            null,
+            null
+        )
 
         Log.d("MainActivity", "cursor ${cursor?.count}")
 
     }
 
-    private fun setupNavigation() {
-        navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        navController = navHostFragment.navController
-        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
-
-        navHostFragment.childFragmentManager.addOnBackStackChangedListener {
-                Log.d("fbm backstack", navHostFragment.childFragmentManager.backStackEntryCount.toString())
-        }
-    }
+//    private fun setupNavigation() {
+//        navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+//        navController = navHostFragment.navController
+//        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
+//
+//        navHostFragment.childFragmentManager.addOnBackStackChangedListener {
+//            Log.d(
+//                "fbm backstack",
+//                navHostFragment.childFragmentManager.backStackEntryCount.toString()
+//            )
+//        }
+//    }
 
     private fun handleOnBackPressed() {
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
@@ -53,7 +77,10 @@ class MainActivity : AppCompatActivity() {
 
                     showExitConfirmationDialog()
                 } else {
-                    Log.d("fbm backstack", navHostFragment.childFragmentManager.backStackEntryCount.toString())
+                    Log.d(
+                        "fbm backstack",
+                        navHostFragment.childFragmentManager.backStackEntryCount.toString()
+                    )
                     navController.popBackStack()
                 }
             }
@@ -71,5 +98,24 @@ class MainActivity : AppCompatActivity() {
                 dialog.dismiss()
             }
             .show()
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    val photoListViewModel: PhotoListViewModel = hiltViewModel()
+
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(navController = navController)
+        }
+    ) { padding ->
+        FbmNavGraph(
+            navController = navController,
+            photoListViewModel = photoListViewModel,
+            modifier = Modifier.padding(padding)
+        )
     }
 }
