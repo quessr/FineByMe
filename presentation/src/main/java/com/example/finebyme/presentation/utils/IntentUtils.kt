62335ac9600793e7be65cb19482ca25e0d67edf9
quessr <heosera76@gmail.com>
2025-04-16 15:28:41 +0900

    필요없는 코드 주석처리

--- presentation/src/main/java/com/example/finebyme/presentation/utils/IntentUtils.kt
+++ presentation/src/main/java/com/example/finebyme/presentation/utils/IntentUtils.kt
@@ -1,15 +1,10 @@
 package com.example.finebyme.presentation.utils
 
-import android.content.Context
-import android.content.Intent
-import com.example.finebyme.domain.entity.Photo
-import com.example.finebyme.presentation.photoDetail.PhotoDetailActivity
-
-object IntentUtils {
-    private const val ARG_PHOTO = "photo"
-    fun newPhotoDetail(context: Context, photo: Photo): Intent {
-        return Intent(context, PhotoDetailActivity::class.java).apply {
-             putExtra(ARG_PHOTO, photo)
-        }
-    }
-}
+//object IntentUtils {
+//    private const val ARG_PHOTO = "photo"
+//    fun newPhotoDetail(context: Context, photo: Photo): Intent {
+//        return Intent(context, PhotoDetailActivity::class.java).apply {
+//             putExtra(ARG_PHOTO, photo)
+//        }
+//    }
+//}
